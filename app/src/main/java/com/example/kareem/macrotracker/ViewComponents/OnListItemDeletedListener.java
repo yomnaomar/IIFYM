e213dfd912a6bced20f0afc11e2f4772511b8deb
Abdu Sah <abdu_sah@hotmail.com>
2016-11-16 22:09:52 +0400

    fuck

--- app/src/main/java/com/example/kareem/macrotracker/ViewComponents/OnListItemDeletedListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.kareem.macrotracker.ViewComponents;
-
-/**
- * Created by Kareem on 9/8/2016.
- */
-public interface OnListItemDeletedListener {
-        public void onItemDeleted();
-}
