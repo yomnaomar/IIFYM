abc86b5c1c124197a344fd5dc907ea920882fdb4
Yomna <yomna.hassan@hotmail.com>
2017-02-20 23:42:31 +0400

    activityAddDailyItem layout and java file

---
+++ IIFYM_Tracker/src/main/java/com/example/kareem/IIFYM_Tracker/Activities/Main/activityAddDailyItem.java
@@ -0,0 +1,14 @@
+package com.example.kareem.IIFYM_Tracker.Activities.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.kareem.IIFYM_Tracker.R;
+
+public class activityAddDailyItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_daily_item);
+    }
+}
