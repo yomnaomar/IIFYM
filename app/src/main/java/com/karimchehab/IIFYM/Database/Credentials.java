efed3f0adad8cb8b9b381330dd0c06be3e15976f
Karim94 <karimchehab94@gmail.com>
2017-06-24 17:47:54 +0300

    Removed Credentials.java Deleted unused activitySelectDailyItem

---
+++ app/src/main/java/com/karimchehab/IIFYM/Database/Credentials.java
@@ -0,0 +1,13 @@
+package com.karimchehab.IIFYM.Database;
+
+/**
+ * Created by Karim on 03-Jun-17.
+ */
+
+public class Credentials {
+    public static final String IIFYM_SERVER_CLIENT_ID = "930699920107-5nnrfreoc6kms6paa9rattehfvf77ofc.apps.googleusercontent.com";
+
+
+    public static final String FATSECRET_API_ACCESS_KEY = "f902675ae3414fba96c95d80fe9670a3";
+    public static final String FATSECRET_SHARED_SECRET = "f847ad12ea6a46338399891a751e6177";
+}
